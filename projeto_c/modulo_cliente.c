1c57cd7408baddf64b87734bd7a3bd71075b2dbf
thays <thaysfmcandido14@gmail.com>
2026-06-14 20:43:51 -0300

    Update modulo_cliente.c

---
+++ projeto_c/modulo_cliente.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void menu_cliente(void) {
+    printf("\n=============================\n");
+    printf("--- MÓDULO DE CLIENTES ---\n");
+    printf("=============================\n");
+    printf("1. Cadastrar Cliente\n");
+    printf("0. Voltar ao Menu Principal\n");
+    printf("=============================\n");
+}
