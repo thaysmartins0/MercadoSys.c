11bc9a0e66b3c6d5085c9c6a5fac717d20606f9b
thays <thaysfmcandido14@gmail.com>
2026-06-14 20:44:24 -0300

    Update modulo_pedido.c

---
+++ projeto_c/modulo_pedido.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void menu_pedido(void) {
+    printf("\n=============================\n");
+    printf("--- MÓDULO DE PEDIDOS ---\n");
+    printf("=============================\n");
+    printf("1. Criar Novo Pedido\n");
+    printf("0. Voltar ao Menu Principal\n");
+    printf("=============================\n");
+}
